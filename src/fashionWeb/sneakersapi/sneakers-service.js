c9ed87b97e81358a2e4cac7bfc41381c2208018d
Mariah Maynard <98000652+mariah-may23@users.noreply.github.com>
2022-12-10 20:45:02 -0500

    ITS WORKING HAHAHA

--- src/fashionWeb/sneakersapi/sneakers-service.js
+++ src/fashionWeb/sneakersapi/sneakers-service.js
@@ -1,26 +1,28 @@
 import axios from "axios";
 
 
-const SEARCH_URL = 'https://sneaker-database-stockx.p.rapidapi.com/getproducts?keywords=yeezy&limit=5?';
+//const SEARCH_URL = 'https://sneaker-database-stockx.p.rapidapi.com/getproducts?keywords=yeezy&limit=5?';
 
 export const findSneakerBySearchTerm = async (term) => {
 
 //    const response = await axios.get('https://sneaker-database-stockx.p.rapidapi.com/getproducts?keywords=yeezy&limit=5?')
 //    return response.data;
 
-    const options = {
-          method: 'GET',
-          url: 'https://sneaker-database-stockx.p.rapidapi.com/getproducts',
-          params: {keywords: 'yeezy', limit: '5'},
-          headers: {
-            'X-RapidAPI-Key': '389bc1f3cdmsh6009a93e5c1e541p10147ajsnd8f843773938',
-            'X-RapidAPI-Host': 'sneaker-database-stockx.p.rapidapi.com'
-          }
-        };
+    const response = await axios.request(
+        {
+            method: 'GET',
+            url: `https://sneaker-database-stockx.p.rapidapi.com/getproducts?keywords=+${term}&limit=12?`,
+            params: {keywords: 'yeezy', limit: '5'},
+            headers: {
+                'X-RapidAPI-Key': '389bc1f3cdmsh6009a93e5c1e541p10147ajsnd8f843773938',
+                'X-RapidAPI-Host': 'sneaker-database-stockx.p.rapidapi.com'
+            }
+        }
+    );
 
-    const response = await axios.request(options);
     console.log(response.data);
     return response.data;
+
     // axios.request(options).then(res => {
     //     console.log(res.data);
     //     return res.data
