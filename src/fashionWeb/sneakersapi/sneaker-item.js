f94d4ed14eb349114c48d25855669d28203972df
Nicolas Derichsweiler <54852601+nderich@users.noreply.github.com>
2022-12-12 20:01:59 -0500

    changed search bar and search results

--- src/fashionWeb/sneakersapi/sneaker-item.js
+++ src/fashionWeb/sneakersapi/sneaker-item.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {useParams, Link} from "react-router-dom";
 import Details from "../details/details-component";
+import {useSelector} from "react-redux";
 
 const SneakerItem = (
     {
@@ -10,6 +11,7 @@ const SneakerItem = (
     }
 ) => {
 
+    // const {currentUser} = useSelector((state) => state.currentUser)
 
     return (
         <div className="card" style={{width: "18rem"}}>
@@ -19,15 +21,14 @@ const SneakerItem = (
                 <p className="card-text">
                     <ul className="list-unstyled">
                         <li>Color: {search.colorway}</li>
-                        <li>Size: {search.size}</li>
                         <li>Brand: {search.brand}</li>
                         {/*CHANGE USER!!!!*/}
-                        <li>User: Blobb</li>
+                        {/*<li>Seller: {currentUser}</li>*/}
                     </ul>
                 </p>
                 <Link
                     to={`/details/${search._id}`}
-                    className="btn btn-primary">
+                    className="btn btn-dark">
                     Details
                 </Link>
                 <h4 className="float-end fw-bolder"> ${search.retailPrice}</h4>
--- src/fashionWeb/sneakersapi/sneakers-search.js
+++ src/fashionWeb/sneakersapi/sneakers-search.js
@@ -2,6 +2,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {findSneakerBySearchTermThunk} from "./sneakers-thunks";
 import React, {useState, useEffect} from "react";
 import SneakerItem from "./sneaker-item";
+import "./sneakers.css";
+import {Link} from "react-router-dom";
 
 
 const SneakersSearch = () => {
@@ -25,87 +27,62 @@ const SneakersSearch = () => {
     return(
 
         <>
-            {/*Old version working*/}
-            {/*    <h1 className="text-bg-primary"> sneakers search </h1>*/}
-                <input
-                    onChange={(e) => {
-                        setSearchTerm(e.target.value)
-                    }}
-                    value={searchTerm}
+            <div className="bg-black">
+                <div className="container bg-black">
+                    <div className="row">
 
-                />
-                <button onClick={() => {
-                    dispatch(findSneakerBySearchTermThunk(searchTerm))
-                }}>Search</button>
+                        <div className="row pb-3 pt-4 ">
+                            <span className="col-1">
+                            <Link to="/">
+                                <button className="bi bi-arrow-left text-white btn btn-dark"> </button>
+                            </Link>
+                                {/*<span className="text-white ps-2 fs-3">Search</span>*/}
+                            </span>
+                            {/*NEW SEARCH, NOT WORKING, FIX LATER*/}
 
+                            <div className="position-relative col-10">
+                            <span className="d-flex">
+                                <input placeholder="Search Sneakers"
+                                       className="form-control me-sm-2"
+                                       onChange={(e) => {
+                                           setSearchTerm(e.target.value)
+                                       }}
+                                       value={searchTerm}
+                                />
+                                <button className="btn btn-dark my-2 my-sm-0"
+                                        onClick={() => {
+                                            dispatch(findSneakerBySearchTermThunk(searchTerm))
+                                        }}>
+                                    Search
+                                </button>
+                            </span>
 
-
-                {/*<ul>*/}
-                {/*    <li> rendering testing </li>*/}
-                {/*    {!loading && sneakers &&*/}
-                {/*        sneakers.map((sneaker) =>*/}
-
-                {/*            <li key={sneaker._id} className="text-black">*/}
-                {/*                {sneaker.shoeName}*/}
-                {/*            </li>*/}
-                {/*        )*/}
-                {/*    }*/}
-                {/*</ul>*/}
-
-            <div className="container">
-                <h4>Search</h4>
-                <div className="row">
-                    {/*NEW SEARCH, NOT WORKING, FIX LATER*/}
-
-                    {/*<div className="position-relative">*/}
-                    {/*    <input placeholder="Search Items"*/}
-                    {/*           className="form-control rounded-pill ps-5"*/}
-                    {/*           onChange={(e) => {*/}
-                    {/*               setSearchTerm(e.target.value)*/}
-                    {/*           }}*/}
-                    {/*           value={searchTerm}*/}
-                    {/*    />*/}
-                    {/*    <i className="bi bi-search position-absolute*/}
-                    {/*   wd-nudge-up"></i>*/}
-                    {/*</div>*/}
-
-                    {/*NEW BUTTON*/}
-                    {/*<input*/}
-                    {/*    onChange={(e) => {*/}
-                    {/*        setSearchTerm(e.target.value)*/}
-                    {/*    }}*/}
-                    {/*    value={searchTerm}*/}
-
-                    {/*/>*/}
-                    {/*<button onClick={() => {*/}
-                    {/*    dispatch(findSneakerBySearchTermThunk(searchTerm))*/}
-                    {/*}}>Search</button>*/}
-
-                    <div>
-                        {/* Search results start*/}
-
-                        {/*<SearchList/>*/}
-
-                        <div className="row">
-                            {!loading && sneakers &&
-                                sneakers.map((item) =>
-                                    <div className="col-12 col-sm-6 col-lg-4 col-xxl-3 mt-3 d-flex align-items-stretch">
-                                        <SneakerItem
-                                            key={item._id}
-                                            search={item}
-                                            />
-                                    </div>
-                                )
-                            }
+                            </div>
                         </div>
 
-                        {/* Search results end*/}
+                        <div>
+                            {/* Search results start*/}
+
+                            {/*<SearchList/>*/}
+
+                            <div className="row">
+                                {!loading && sneakers &&
+                                    sneakers.map((item) =>
+                                        <div className="col-12 col-sm-6 col-lg-4 col-xxl-3 mt-3 d-flex align-items-stretch">
+                                            <SneakerItem
+                                                key={item._id}
+                                                search={item}
+                                            />
+                                        </div>
+                                    )
+                                }
+                            </div>
+
+                            {/* Search results end*/}
+                        </div>
                     </div>
                 </div>
             </div>
-
-
-
         </>
     )
 }
