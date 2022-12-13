efa64c9edbde47f6f5f978ce6bc5a6ac4485a45a
Nicolas Derichsweiler <54852601+nderich@users.noreply.github.com>
2022-12-13 11:21:23 -0500

    test commit

---
+++ src/fashionWeb/popular-sneakersapi/popular-sneaker-item.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+const PopularSneakerItem = (
+    {
+        search = {
+
+        }
+    }
+) => {
+    console.log("In the Popular sneaker item")
+    console.log(search)
+
+
+    return (
+        <div className="card " style={{width: "18rem"}}>
+            <img className="card-img-top h-100" src={search.thumbnail} alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">{search.shoeName}</h5>
+                <p className="card-text">
+                    <ul className="list-unstyled">
+                        <li>Color: {search.colorway}</li>
+                        <li>Size: {search.size}</li>
+                        <li>Brand: {search.brand}</li>
+                        {/*CHANGE USER!!!!*/}
+                        <li>User: Blobb</li>
+                    </ul>
+                </p>
+                <Link
+                    to={`/details/${search._id}`}
+                    className="btn btn-dark">
+                    Details
+                </Link>
+                <h4 className="float-end fw-bolder"> ${search.retailPrice}</h4>
+            </div>
+        </div>
+    );
+}
+
+export default PopularSneakerItem;
---
+++ src/fashionWeb/popular-sneakersapi/popular-sneakers-search.js
@@ -0,0 +1,57 @@
+import {useDispatch, useSelector} from "react-redux";
+import {findSneakersPopularThunk} from "./popular-sneakers-thunks";
+import React, {useState, useEffect} from "react";
+import PopularSneakerItem from "./popular-sneaker-item";
+
+
+const PopularSneakersSearch = () => {
+
+    const {sneakers, loading} = useSelector((state) => state.sneakers)
+
+    //call thunk with dispatch to find by term
+    const dispatch = useDispatch()
+    console.log(sneakers)
+
+    // THIS IS NOT PROPER USEEFFECT SO DISREGARD OR UPDATE LATER
+    useEffect(() => {
+            dispatch(findSneakersPopularThunk())
+
+    }, [])
+
+
+
+    return(
+
+        <>
+
+
+            <div className="bg-black">
+                {/*<div className="row container-fluid bg-black">*/}
+
+                    <div className="container bg-black">
+                        <div className="row">
+                            <h1 className="fw-bolder text-white pt-4 ps-4">Suggestions curated just for you:</h1>
+                            {!loading && sneakers &&
+                                sneakers.map((item) =>
+                                    <div className="col-12 col-sm-6 col-lg-4 col-xxl-3 mt-3 d-flex align-items-stretch
+                                    bg-black p-4">
+                                        <PopularSneakerItem
+                                            key={item._id}
+                                            search={item}
+                                            />
+                                    </div>
+                                )
+                            }
+                        </div>
+
+
+                    </div>
+                {/*</div>*/}
+            </div>
+
+
+
+        </>
+    )
+}
+export default PopularSneakersSearch;
